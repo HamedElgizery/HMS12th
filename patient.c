f747a777812155057cb8faf3c2a6c9d0e5d96636
Shahd Adel <shahdadel2805@gmail.com>
2023-05-18 16:46:22 +0200

    Struct Patient

---
+++ patient.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+struct patient
+{
+    char name[100];
+    int patient_ID;
+    int age;
+    char address[100];
+    char gender;
+    char blood_type[4];
+    char disease[100];
+    int specialist_roomno;
+    int fam_contact[20];
+
+};
