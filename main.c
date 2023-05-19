b055fdd589522c32c5045f69f77bdb0872f5a730
Hamed Elgizery <hamedashraf2004@gmail.com>
2023-05-19 12:59:31 +0300

    add patient struct

--- main.c
+++ main.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char* generate_id() {
-  char id[4] = {'0', '0', '0', '0'};
-  
+struct patient
+{
+    char name[100];
+    int patient_ID;
+    int age;
+    char address[100];
+    char gender;
+    char blood_type[4];
+    char disease[100];
+    int specialist_roomno;
+    int fam_contact[20];
+
+};
+
+const char* generate_id() {
+  static char id[4];
+  for (int i = 0; i < 4; ++i) {
+    int num = rand() % 10;
+    if (i + num) id[i] = num + '0';
+    else --i;
+  }
+  for (int i = 0; i < 4; ++i) {
+    printf("%c", id[i]);
+  }
   return id;
 }
 
@@ -28,6 +50,6 @@ void delete_patient() {
 }
 
 int main() {
-       
+  puts(generate_id());
   return 0;
 }
