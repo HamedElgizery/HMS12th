c6995b8360371962530109f9f7c680deafb3cca7
zeina <catsdisownedz@gmail.com>
2023-05-19 12:40:46 +0300

    menu.c
    display menu for hms

---
+++ body incubators.c
@@ -0,0 +1,53 @@
+//main appearance menu 
+#include<stdio.h>
+void menu(){
+    int choice = 0; 
+    do{
+        headMessage("HOSPITAL MANAGEMENT SYSTEM\n");
+        
+        printf("\t\t\t1) Track intensive care\n");
+        printf("\t\t\t2) Track newborn babies\n");
+        printf("\t\t\t3) Check which patients occupy what department\n");
+        printf("\t\t\t4) Check quantity of a blood type\n");
+        printf("\t\t\t5) Add a new patient record\n");
+        printf("\t\t\t6) Edit existing patient record\n");
+        printf("\t\t\t7) Show patient list and info\n");
+        printf("\033[0;31m");
+        printf("\t\t\t8) Delete a patient record\n");
+        printf("\033[0m");
+        printf("---- Press zero to exit ----");
+        ///////////
+        scanf("%d", &choice);
+        switch(choice){
+            case 1: 
+                Track_intensive_care();
+                break;
+            case 2:
+                Track_baby_incubators();
+                break;
+            case 3:
+                Hospital_capacity();
+                break;
+            case 4:
+                Track_blood_types();
+                break;
+            case 5:
+                Add_patients();
+                break;
+            case 6:
+                Edit_patients();
+                break;
+            case 7:
+                list_patients();
+                break;
+            case 8:
+                Delete_patients();
+                break;
+            case 0:
+                printf("You are now exiting the menu...");
+                sleep(3);
+                exit(1);
+        }
+    }
+    while(choice!=0);
+}
