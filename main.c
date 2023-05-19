f74bcdffc0251d502cc74ef1f0c356e77f865603
Hamed Elgizery <hamedashraf2004@gmail.com>
2023-05-19 12:42:19 +0300

    add main

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* generate_id() {
+  char id[4] = {'0', '0', '0', '0'};
+  
+  return id;
+}
+
+void add_patient() {
+  
+}
+
+void hospital_capacity() {
+
+}
+
+void edit_patient() {
+
+}
+
+void list_patients() {
+
+}
+
+void delete_patient() {
+
+}
+
+int main() {
+       
+  return 0;
+}
