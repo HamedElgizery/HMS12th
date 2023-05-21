56719a87e83b2cbceff1ece176f6bcf6475f248a
zeina <catsdisownedz@gmail.com>
2023-05-21 15:12:00 +0300

    track baby incubators

---
+++ editrecord.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+edit_record(){
+    FILE *fp = 0;
+    int found= 0;
+    headMessage("Enter patient name to edit record");
+    fp= fopen("file.txt", "rb");
+    if (fseek(fp, ))
+
+
+
+
+    if (!found){
+        printf("Record is not found.");
+    }
+    printf("Press any key to go to the main menu..");
+    getchar();
+    //
+
+
+
+}
---
+++ track_baby_incubators.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int count_newborn_patients(){
+    int count =0;
+    char line[30];
+    FILE *fp;
+    fopen("D://patients_info.txt", "r");
+    if (fp == 0){
+        printf("Unable to open file\n");
+        return -1;
+    }
+    while(fgets(line, sizeof(line), fp) != 0){
+        if(strcmp(line, "newborn")!=0)
+        count++;
+        }
+    fclose(fp);
+    return count;
+}
+
+
+int track_baby_incubators(int free_beds){
+    int babies = count_newborn_patients();
+    return 560 - babies;
+}
+
